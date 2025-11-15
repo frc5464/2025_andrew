1e2231e52bcc12b7e91f433c7d243360317f72ff
FRC5464 <94130236+frc5464@users.noreply.github.com>
2025-11-15 12:13:34 -0600

    CommandFactory created

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -2,10 +2,17 @@
 
 package frc.robot;
 
+import frc.robot.commands.ButterIntakeCommand;
+import frc.robot.commands.ButterOuttakeCommand;
+import frc.robot.commands.ButterTiltCommand;
+import frc.robot.commands.DriveCommand;
+import frc.robot.commands.IntakeCommand;
+import frc.robot.commands.ShooterCommand;
 import frc.robot.subsystems.ButterClawSubsystem;
 import frc.robot.subsystems.DriveTrainSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.utils.CommandFactory;
 import frc.robot.utils.SubsystemManager;
 
 //Move forward
@@ -15,13 +22,23 @@ import frc.robot.utils.SubsystemManager;
 //Backup
 public class Autos{
     
-    public Autos(SubsystemManager subsystemManager){
+    public Autos(SubsystemManager subsystemManager, CommandFactory commandFactory){
+        //Subsystems
         final IntakeSubsystem intake = subsystemManager.getIntakeSubsystem();
         final ShooterSubsystem shooter = subsystemManager.getShooterSubsystem();
         final ButterClawSubsystem butterClaw = subsystemManager.getButterClawSubsystem();
         final DriveTrainSubsystem driveTrain = subsystemManager.getDriveTrainSubsystem();
-
+        
+        //Commands
+        final DriveCommand driveCommand = commandFactory.getDriveCommand();
+        final IntakeCommand intakeCommand = commandFactory.getIntakeCommand();
+        final ShooterCommand shooterCommand = commandFactory.getShooterCommand();
+        final ButterIntakeCommand butterIntakeCommand = commandFactory.getButterIntakeCommand();
+        final ButterOuttakeCommand butterOuttakeCommand = commandFactory.getButterOuttakeCommand();
+        final ButterTiltCommand butterTiltCommand = commandFactory.getButterTiltCommand();
     }
+
+    
 
 
 
--- src/main/java/frc/robot/utils/CommandFactory.java
+++ src/main/java/frc/robot/utils/CommandFactory.java
@@ -1,5 +1,45 @@
 package frc.robot.utils;
 
+import frc.robot.commands.ButterIntakeCommand;
+import frc.robot.commands.ButterOuttakeCommand;
+import frc.robot.commands.ButterTiltCommand;
+import frc.robot.commands.DriveCommand;
+import frc.robot.commands.IntakeCommand;
+import frc.robot.commands.ShooterCommand;
+
 public class CommandFactory {
-    
+    private final DriveCommand driveCommand = new DriveCommand(null, null);
+    private final IntakeCommand intakeCommand = new IntakeCommand(null);
+    private final ShooterCommand shooterCommand = new ShooterCommand(null);
+    private final ButterIntakeCommand butterIntakeCommand = new ButterIntakeCommand(null);
+    private final ButterOuttakeCommand butterOuttakeCommand = new ButterOuttakeCommand(null);
+    private final ButterTiltCommand butterTiltCommand = new ButterTiltCommand(null);
+
+    public CommandFactory(){
+
+    }
+
+    public DriveCommand getDriveCommand(){
+       return driveCommand;
+    }
+
+    public IntakeCommand getIntakeCommand(){
+        return intakeCommand;
+    }
+
+    public ShooterCommand getShooterCommand(){
+        return shooterCommand;
+    }
+
+    public ButterIntakeCommand getButterIntakeCommand(){
+        return butterIntakeCommand;
+    }
+
+    public ButterOuttakeCommand getButterOuttakeCommand(){
+        return butterOuttakeCommand;
+    }
+
+    public ButterTiltCommand getButterTiltCommand(){
+        return butterTiltCommand;
+    }
 }
