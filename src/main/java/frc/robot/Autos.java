376077823a67d8f24f072718a40b1c1ae01649e5
FRC5464 <94130236+frc5464@users.noreply.github.com>
2025-11-15 11:42:42 -0600

    autos work

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -1,6 +1,32 @@
-package frc.robot;
-import edu.wpi.first.wpilibj.TimedRobot;
 
-public class Autos extends TimedRobot{
+
+package frc.robot;
+
+import frc.robot.subsystems.ButterClawSubsystem;
+import frc.robot.subsystems.DriveTrainSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.utils.SubsystemManager;
+
+//Move forward
+//shoot popcorn
+//rotate to melter
+//vomit butter
+//Backup
+public class Autos{
     
+    public Autos(SubsystemManager subsystemManager){
+        final IntakeSubsystem intake = subsystemManager.getIntakeSubsystem();
+        final ShooterSubsystem shooter = subsystemManager.getShooterSubsystem();
+        final ButterClawSubsystem butterClaw = subsystemManager.getButterClawSubsystem();
+        final DriveTrainSubsystem driveTrain = subsystemManager.getDriveTrainSubsystem();
+
+    }
+
+
+
+
+
+
+
 }
